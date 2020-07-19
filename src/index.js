67067ca80ac41ebe671b057d53bae4cc1494d0e3
Sơn Ngọc <sondnpt00343@gmail.com>
2020-07-20 01:30:40 +0700

    Static files & SASS

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // HTTP logger
 app.use(morgan('combined'));
 
