e623bf759eff585a50a73efea9a4772b2c85e8d0
Sơn Ngọc <sondnpt00343@gmail.com>
2020-07-19 23:57:55 +0700

    Install morgan

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+app.use(morgan('combined'))
+
 app.get('/tin-tuc', (req, res) => {
-    var a = 1;
-    var b = 2;
-
-    var c = a + b;
-
     return res.send('Hello World!');
 })
 
