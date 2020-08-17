830aa25fb858873522f4da5150f795dd729d47e0
Sơn Ngọc <sondnpt00343@gmail.com>
2020-08-17 10:16:40 +0700

    Count documents deleted

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,9 +4,10 @@ const { mutipleMongooseToObject } = require('../../util/mongoose');
 class MeController {
     // [GET] /me/stored/courses
     storedCourses(req, res, next) {
-        Course.find({})
-            .then((courses) =>
+        Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+            .then(([courses, deletedCount]) =>
                 res.render('me/stored-courses', {
+                    deletedCount,
                     courses: mutipleMongooseToObject(courses),
                 }),
             )
